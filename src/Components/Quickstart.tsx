f9a73fe5d355f6d753170b7e57bb99d82150e411
rabab003 <bintaliraq4@gmail.com>
2025-02-20 22:32:28 +0300

    20 feb

--- src/Components/Quickstart.tsx
+++ src/Components/Quickstart.tsx
@@ -10,8 +10,8 @@ const Quickstart = () => {
   };
 
   return (
-    <div>
-      <div className="flex items-start justify-center h-full p-5  rounded bg-mainBlack [&::-webkit-scrollbar]:w-2
+    <div className=''>
+      <div className="flex md:p-12 items-start justify-center p-5 rounded bg-mainBlack overflow-y-scroll h-full [&::-webkit-scrollbar]:w-2
       [&::-webkit-scrollbar-track]:rounded-full
       [&::-webkit-scrollbar-track]:bg-gray-100
       [&::-webkit-scrollbar-thumb]:rounded-full
@@ -19,19 +19,19 @@ const Quickstart = () => {
       dark:[&::-webkit-scrollbar-track]:bg-neutral-700
       dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
 
-        <div className="flex flex-col md:flex-row">
+        <div className="flex flex-col md:flex-row ">
 
           <div>
 
             <h1 className="text-3xl font-semibold">Developer quickstart</h1>
             <h3 className="py-2 text-base text-darkText">Learn how to make your first API request.</h3>
-            <p className="text-[15px] pt-7">The OpenAI API provides a simple interface to state-of-the-art AI <span className="text-Lightgreen">models</span>  for natural language processing, image generation, semantic search, and speech recognition. Follow this guide to learn how to generate human-like responses to <span className="text-Lightgreen">natural language prompts, create vector embeddings </span> for semantic search, and <span className="text-Lightgreen"> generate images </span> from textual descriptions.</p>
+            <p className="text-[15px] font-normal pt-7 text-base/7">The OpenAI API provides a simple interface to state-of-the-art AI <span className="text-Lightgreen">models</span>  for natural language processing, image generation, semantic search, and speech recognition. Follow this guide to learn how to generate human-like responses to <span className="text-Lightgreen">natural language prompts, create vector embeddings </span> for semantic search, and <span className="text-Lightgreen"> generate images </span> from textual descriptions.</p>
 
 
             <div className="py-10">
               <div>
                 <h2 className="pb-5 text-2xl">Create and export an API key </h2>
-                <p className="font-light"><span className="text-Lightgreen">Create an API key in the dashboard here</span>, which you’ll use to securely <span className="text-Lightgreen"> access the API</span>. Store the key in a safe location, span like a <a className="text-Lightgreen"> .zshrc file </a>or another text file on your computer. Once you’ve generated an API key, export it as an <span className="text-Lightgreen">environment variable</span>  in your terminal.</p>
+                <p className="font-normal text-base/7"><span className="text-Lightgreen">Create an API key in the dashboard here</span>, which you’ll use to securely <span className="text-Lightgreen"> access the API</span>. Store the key in a safe location, span like a <a className="text-Lightgreen"> .zshrc file </a>or another text file on your computer. Once you’ve generated an API key, export it as an <span className="text-Lightgreen">environment variable</span>  in your terminal.</p>
                 <div className="flex mt-4 space-x-4 the_buttons">
                  <div className='p-1 bg-black rounded-md'>
                   <button
@@ -83,7 +83,7 @@ const Quickstart = () => {
 
               <div className='pt-8'>
                 <h2 className='pb-3 text-2xl font-bold'>Make your first API request</h2>
-                <p className='text-darkText'>With your OpenAI API key exported as an environment variable, you're ready to make your first API request. You can either use the <a href='#' className='text-green'>REST API</a>  directly with the HTTP client of your choice, or use one of our <a href="" className='text-green'> official SDKs</a> as shown below.</p>
+                <p className='text-darkText text-base/7'>With your OpenAI API key exported as an environment variable, you're ready to make your first API request. You can either use the <a href='#' className='text-green'>REST API</a>  directly with the HTTP client of your choice, or use one of our <a href="" className='text-green'> official SDKs</a> as shown below.</p>
                 
               </div>
 
@@ -92,7 +92,7 @@ const Quickstart = () => {
 
           </div>   
 
-          <div className="hidden md:block">
+          <div className="hidden lg:block">
             <h1>Create and export an API key</h1>
             <h1> Make your first API request</h1>
             <h1>Next steps</h1>
